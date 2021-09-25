e18af0bd861f0b77bf74041aacc51f06f9a1fd6d
Sumit19jan <sumitsingh7050@gmail.com>
2021-09-25 11:10:12 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/makemytrip/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.makemytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.makemytrip.splashScreen.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
